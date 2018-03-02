01079b3713cd39c0f6c779c78ad3d9f048b8c182
Anish Manocha <anishmanocha@gmail.com>
2018-03-01 22:32:06 -0500

    learned about bean lifecycles, now learning about hooks to add after bean creation and on bean destruction

---
+++ spring-demo-one/src/com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,32 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		//Establish connection with Spring Container
+		ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext("beanScopeApplicationContext.xml");
+		//Retrieve apporpriate beans
+		Coach coach= context.getBean("trackCoach", Coach.class);
+		
+		Coach sameCoach= context.getBean("trackCoach", Coach.class);
+		
+		if (coach == sameCoach) {
+			
+			System.out.print("This proves that the life span of a bean is by default that of a singleton");
+		}
+		
+		/*If we change the scope to be prototype, aka, every time we grab a coach from the Spring container, it's a different coach,
+		we'll be pointing to different places in memory*/
+		System.out.println("Memory location of first coach " + coach);
+		System.out.println("Memory location of other coach " + sameCoach);
+		
+		//Close connection with Spring container
+		
+		context.close();
+
+	}
+
+}
